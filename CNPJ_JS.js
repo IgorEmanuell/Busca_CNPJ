a630209406d9a398487b810559b98774b1e9c613
Igor Emanuell <emanuelligor@hotmail.com>
2024-02-01 20:02:42 -0300

    Adicionando tempo para o codigo js

--- CNPJ_JS.js
+++ CNPJ_JS.js
@@ -7,11 +7,19 @@ let df = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
 let cnpjs = df.map(row => row.CNPJ);
 
-Promise.all(cnpjs.map(cnpj => {
-    return axios.get(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`)
-        .then(response => {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchCNPJData(cnpjs) {
+    let results = [];
+    for (let i = 0; i < cnpjs.length; i++) {
+        let cnpj = cnpjs[i];
+        try {
+            let response = await axios.get(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`);
+            console.log(`Status da resposta para o CNPJ ${cnpj}: ${response.status}`);
             let data = response.data;
-            return {
+            results.push({
                 NOME: data.nome || 'N/A',
                 FANTASIA: data.fantasia || 'N/A',
                 PORTE: data.porte || 'N/A',
@@ -21,20 +29,24 @@ Promise.all(cnpjs.map(cnpj => {
                 UF: data.uf || 'N/A',
                 CEP: data.cep || 'N/A',
                 EMAIL: data.email || 'N/A',
-                TELEFONE: data.telefone || 'N/A'
-            };
-        })
-        .catch(error => {
-            console.error(`Erro ao buscar dados para o CNPJ ${cnpj}: ${error}`);
-            return null;
-        });
-})).then(results => {
+                TELEFONE: data.telefone || 'N/A',
+            });
+        } catch (error) {
+            console.log(`Erro ao buscar o CNPJ ${cnpj}: ${error.response.status}`);
+        }
+        if ((i + 1) % 3 === 0) {
+            await sleep(70000);
+        }
+    }
+    return results;
+}
+fetchCNPJData(cnpjs).then(results => {
     results = results.filter(result => result !== null);
-
+    
     df = df.concat(results);
 
     let new_workbook = XLSX.utils.book_new();
     let new_worksheet = XLSX.utils.json_to_sheet(df);
     XLSX.utils.book_append_sheet(new_workbook, new_worksheet, 'Sheet1');
-    XLSX.writeFile(new_workbook, 'Planilha_CNPJ2.xlsx');
+    XLSX.writeFile(new_workbook, 'Planilha_CNPJ.xlsx');
 });
